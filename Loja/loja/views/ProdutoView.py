442af473cf2ef46c2530c06b411ef0a4427a69dc
Luca Hashiguchi Medeiros <20241011110011@ifrn.local>
2026-09-14 10:18:41 -0300

    capitulo 11 eu acho

--- Loja/loja/views/ProdutoView.py
+++ Loja/loja/views/ProdutoView.py
@@ -2,10 +2,11 @@ from datetime import timedelta
 from django.utils import timezone
 from django.shortcuts import render, redirect
 from django.core.files.storage import FileSystemStorage
+from django.contrib.auth.decorators import login_required
 
 from loja.models import Categoria, Fabricante, Produto
 
-
+@login_required
 def edit_produto_view(request, id=None):
     produtos = Produto.objects.all()
     if id is not None:
